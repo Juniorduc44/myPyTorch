f62d50c3a532730da904351c3bed9437379383d0
BigDaddyDuuk <bigdaddyduuk@pop-os.localdomain>
2024-03-05 21:14:08 -0800

    v0.0.2

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-#v0.0.1
-
+#v0.0.2
 import torch
 
+
 a = torch.cuda.is_available()
-b = torch.device(1)
-c = torch.cuda.memory_reserved(device=0)
-d = torch.cuda.memory_reserved(device=1)
-e = torch.cuda.memory_stats(device=0)
-f = torch.cuda.memory_summary(torch.device, abbreviated=False)
+aa = torch.cuda.device_count()
+aaa = torch.cuda.current_device()
+b = torch.device(0)
+c = torch.cuda.memory_reserved(device=0) #should be the first gpu
+#d = torch.cuda.memory_reserved(device=1) #assuming you have another gpu
+e = torch.cuda.memory_stats(device=0) #status of the first gpu
+f = torch.cuda.memory_summary(torch.device, abbreviated=False) 
 
 
 
 
 
 print(b)
+print(f"Amount of GPUs == {aa}")
+print(f"Current device == {aaa}")
 print(f"State of CUDA == {a}")
-print(f"Memory Reserved on GPU1 = {c}")
-print(f"Memory Reserved on GPU2 = {d}")
-print(f"Memory stats for GPU1 = {e}")
-print(f"Statistics on GPU1 = {f}")
+print(f"Memory Reserved on GPU{000} = {c}")
+#print(f"Memory Reserved on GPU2 = {d}")
+print(f"Memory stats for GPU{aaa} = {e}")
+print(f"Statistics on GPU{aaa} = {f}")
 #print(f"Max Memory Reserved = {max_memory_reserved()}")
